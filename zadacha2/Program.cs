0060ea7e3370756e259ec56ac19345fe620a9b3f
Pavel B <boronenkov999@mail.ru>
2023-01-17 22:19:25 +0300

    ДЗ на поиск <>

---
+++ zadacha2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число А ");
+int numbersA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int numbersB = Convert.ToInt32(Console.ReadLine());
+
+if (numbersA < numbersB)
+{
+    Console.Write("max = ");
+    Console.Write(numbersB);
+    Console.Write(" min = ");
+    Console.Write(numbersA);
+}
+
+else
+{
+    Console.Write("max = ");
+    Console.Write(numbersA);
+    Console.Write(" min = ");
+    Console.Write(numbersB);
+}
