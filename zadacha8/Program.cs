e38ff3d759e96378eaeb91114e28a332efb586fe
Pavel B <boronenkov999@mail.ru>
2023-01-20 15:23:10 +0300

    финальное сохранение

--- zadacha8/Program.cs
+++ zadacha8/Program.cs
@@ -1,12 +1,22 @@
-﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+﻿
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
 Console.Write("введие число: ");
 int numbersA = Convert.ToInt32(Console.ReadLine());
 int numbersB = 1;
 
 while (numbersB != numbersA)
 {
+    if (numbersB < numbersA)
+    {
     if (numbersB % 2 == 0)
+
     Console.WriteLine(numbersB);
     numbersB += 1;
-
+    }
+    else
+    {
+        Console.WriteLine("меньше 1");
+        break;
+    }
 }
